d3fec3e7daf2359ab388904e5f284eed3fff7855
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-03-23 12:42:25 +0300

    Added guid.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Информация по номерам, объявления avito olx, кто звонил">
     <title>@yield('title')</title>
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/bootstrap-material-design.min.css" rel="stylesheet">
@@ -29,5 +30,6 @@
 <script src="/js/jquery.min.js"></script>
 <script src="/js/material.min.js"></script>
 @include('metrika')
+<div style="color: rgba(0,0,0,0.4); font-size: x-small">098e29b339e6485db77c733599e2005b</div>
 </body>
 </html>
