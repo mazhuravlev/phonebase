f4eb7df39164d3aba0fbbea461f1735aee317485
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-03-24 01:29:49 +0300

    vk comments

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,6 +27,19 @@
         @endforeach
     </ul>
 
+    <!-- Put this script tag to the <head> of your page -->
+    <script type="text/javascript" src="//vk.com/js/api/openapi.js?121"></script>
+
+    <script type="text/javascript">
+        VK.init({apiId: 5174401, onlyWidgets: true});
+    </script>
+
+    <!-- Put this div tag to the place, where the Comments block will be -->
+    <div id="vk_comments" style="width: 100%;"></div>
+    <script type="text/javascript">
+        VK.Widgets.Comments("vk_comments", {limit: 10, attach: "*"}, "index");
+    </script>
+
     <ul class="no-style">
         @foreach($phones as $phone)
             <li>
--- resources/views/phone.blade.php
+++ resources/views/phone.blade.php
@@ -33,6 +33,19 @@
         </div>
     @endif
 
+            <!-- Put this script tag to the <head> of your page -->
+        <script type="text/javascript" src="//vk.com/js/api/openapi.js?121"></script>
+
+        <script type="text/javascript">
+            VK.init({apiId: 5174401, onlyWidgets: true});
+        </script>
+
+        <!-- Put this div tag to the place, where the Comments block will be -->
+        <div id="vk_comments" style="width: 100%;"></div>
+        <script type="text/javascript">
+            VK.Widgets.Comments("vk_comments", {limit: 10, attach: "*"}, "{{ $phone->number }}");
+        </script>
+
     <div class="bs-component">
         <ul class="pager">
             <li class="previous @if(!$prev) disabled @endif"><a
