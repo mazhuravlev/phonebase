502eef12559faa6752032bf60ea08d8e8096f9cd
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-05-25 11:46:56 +0300

    added  banners

---
+++ resources/views/banners/big1.blade.php
@@ -0,0 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- big1 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:336px;height:280px"
+     data-ad-client="ca-pub-2059436417027678"
+     data-ad-slot="3819081941"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
---
+++ resources/views/banners/long.blade.php
@@ -0,0 +1,9 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- long -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:600px"
+     data-ad-client="ca-pub-2059436417027678"
+     data-ad-slot="5295815147"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
